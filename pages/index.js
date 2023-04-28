402fde6802011a0a26ea30cbaae78b45c5b9a3cf
sheyax <olusheyax@gmail.com>
2023-04-28 15:59:11 +0000

    new slider

--- pages/index.js
+++ pages/index.js
@@ -97,8 +97,12 @@ export default function Home() {
           </span>
           m/s
         </p> */}
-
-        <Slider aria-label="Speed" value={speed?.toFixed(1)} />
+        <div className="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+          <div
+            className="bg-blue-600 h-1.5 rounded-full dark:bg-blue-500"
+            style={{ width: speed }}
+          ></div>
+        </div>
       </div>
     </div>
   );
